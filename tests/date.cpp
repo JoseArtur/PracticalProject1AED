26a4b514e41a02831ee1e180e8d4fdd7a3e3b964
GuiPereira <93599708+GuiPereira77@users.noreply.github.com>
2021-12-06 18:44:47 +0000

    Date.cpp

--- tests/date.cpp
+++ tests/date.cpp
@@ -2,3 +2,40 @@
 // Created by gdrp7 on 06/12/2021.
 //
 
+#include "date.h"
+
+Date::Date(unsigned int day, unsigned int month, unsigned int year, unsigned int hour, unsigned int minute) {
+    this->day = day;
+    this->month = month;
+    this->year = year;
+    this->hour = hour;
+    this->minute = minute;
+}
+
+unsigned Date::getHour() const {
+    return this->hour;
+}
+
+unsigned Date::getMinute() const {
+    return this->minute;
+}
+
+unsigned Date::getDay() const {
+    return this->day;
+}
+
+unsigned Date::getMonth() const {
+    return this->month;
+}
+
+unsigned Date::getYear() const{
+    return this->year;
+}
+
+Date::Date() {
+    this->day = 0;
+    this->month = 0;
+    this->year = 0;
+    this->hour = 0;
+    this->minute = 0;
+}
