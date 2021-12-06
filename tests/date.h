279a5db1de59a1a14f3ccc3ba23b981255da9b93
GuiPereira <93599708+GuiPereira77@users.noreply.github.com>
2021-12-06 18:44:18 +0000

    Date.h

--- tests/date.h
+++ tests/date.h
@@ -5,4 +5,20 @@
 #ifndef PLANESERVICE_CPP_DATE_H
 #define PLANESERVICE_CPP_DATE_H
 
+class Date {
+    unsigned hour;
+    unsigned minute;
+    unsigned day;
+    unsigned month;
+    unsigned year;
+public:
+    Date();
+    Date(unsigned day, unsigned month, unsigned year, unsigned hour, unsigned minute);
+    unsigned getHour() const;
+    unsigned getMinute() const;
+    unsigned getDay() const;
+    unsigned getMonth() const;
+    unsigned getYear() const;
+};
+
 #endif //PLANESERVICE_CPP_DATE_H
